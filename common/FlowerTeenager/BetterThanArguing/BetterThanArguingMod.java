3b67f89d8dde26a155bde85db7c31752386d4c8a
FlowerTeenager <flowerteenager.mc@gmail.com>
2013-07-12 04:01:07 -0600

    Basic Config handling

--- common/FlowerTeenager/BetterThanArguing/BetterThanArguingMod.java
+++ common/FlowerTeenager/BetterThanArguing/BetterThanArguingMod.java
@@ -1,5 +1,9 @@
 package FlowerTeenager.BetterThanArguing;
 
+import java.util.logging.Level;
+
+import net.minecraftforge.common.Configuration;
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -12,7 +16,20 @@ public class BetterThanArguingMod {
 	@EventHandler
 	public void PreInit (FMLPreInitializationEvent event)
 	{
-		
+		Configuration cfg = new Configuration(event.getSuggestedConfigurationFile());
+        try
+        {
+            cfg.load();
+        }
+        catch (Exception e)
+        {
+            FMLLog.log(Level.SEVERE, e, "Better than Arguing has a problem loading it's configuration");
+        }
+        finally
+        {
+            if (cfg.hasChanged())
+                cfg.save();
+        }
 	}
 	
 	@EventHandler
