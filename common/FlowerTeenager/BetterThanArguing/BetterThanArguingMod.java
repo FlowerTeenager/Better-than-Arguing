f4c42fd18e82cabc8cce35ddebc9c77927dbcac4
FlowerTeenager <flowerteenager.mc@gmail.com>
2013-07-12 02:22:09 -0600

    Added central mod class

---
+++ common/FlowerTeenager/BetterThanArguing/BetterThanArguingMod.java
@@ -0,0 +1,30 @@
+package FlowerTeenager.BetterThanArguing;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "BetterThanArguing", name = "Better Than Arguing", version = "0")
+public class BetterThanArguingMod {
+	
+	@EventHandler
+	public void PreInit (FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void Init (FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void PostInit (FMLPostInitializationEvent event)
+	{
+		
+	}
+
+}
