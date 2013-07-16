1bdee83a01c324debf11595935e842d7afd38724
FlowerTeenager <flowerteenager.mc@gmail.com>
2013-07-16 14:30:32 -0600

    I think I got it...

--- common/FlowerTeenager/BetterThanArguing/item/food/CreeperOyster.java
+++ common/FlowerTeenager/BetterThanArguing/item/food/CreeperOyster.java
@@ -4,6 +4,8 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -19,10 +21,20 @@ public class CreeperOyster extends ItemFood {
 	}
 	
 	public ItemStack onItemRightClick(ItemStack item, World world, EntityPlayer player) {
+		PotionEffect potion;
 		if (player.canEat(false))
         {
             player.setItemInUse(item, this.getMaxItemUseDuration(item));
         }
         return item;
+	}
+	
+	public void onFoodEaten (ItemStack stack, World world, EntityPlayer player) {
+		int duration = 4;
+		PotionEffect potion;
+		potion = player.getActivePotionEffect(Potion.hunger);
+		if (potion != null)
+			duration = potion.duration;
+		player.addPotionEffect(new PotionEffect(Potion.harm.id, duration));
 	}
 }
