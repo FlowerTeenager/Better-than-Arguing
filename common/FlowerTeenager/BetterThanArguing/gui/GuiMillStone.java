04484741ff02e15f41ada8d5d67be1b4f19c8d01
FlowerTeenager <flowerteenager.mc@gmail.com>
2013-07-19 16:41:16 -0600

    Strings!

--- common/FlowerTeenager/BetterThanArguing/gui/GuiMillStone.java
+++ common/FlowerTeenager/BetterThanArguing/gui/GuiMillStone.java
@@ -14,9 +14,17 @@ public class GuiMillStone extends GuiContainer {
 	public GuiMillStone (InventoryPlayer inventoryPlayer, TileMillStone tileEntity) {
 		super(new ContainerMillStone(inventoryPlayer, tileEntity));
 	}
+	
+	@Override
+	public void drawGuiContainerForegroundLayer(int par1, int par2)
+	{
+		fontRenderer.drawString("Mill Stone", 8, 6, 0x404040);
+		fontRenderer.drawString("Inventory", 8, (ySize - 96) + 2, 0x404040);
+	}
 
 	@Override
-	protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
+	public void drawGuiContainerBackgroundLayer(float f, int i, int j)
+	{
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 	    this.mc.renderEngine.func_110577_a(new ResourceLocation("betterthanarguing:textures/gui/mill_stone.png"));
 	    int x = (width - xSize) / 2;
