6442781f0c3912d8af0404a5536287b2f83757f3
FlowerTeenager <flowerteenager.mc@gmail.com>
2013-07-23 19:32:54 -0600

    Put the model in a location that makes more sense

--- common/FlowerTeenager/BetterThanArguing/model/ModelHandCrank.java
+++ common/FlowerTeenager/BetterThanArguing/model/ModelHandCrank.java
@@ -1,4 +1,4 @@
-package FlowerTeenager.BetterThanArguing.render;
+package FlowerTeenager.BetterThanArguing.model;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
